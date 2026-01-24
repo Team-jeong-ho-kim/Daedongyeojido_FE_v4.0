22d58d2dfc2f09a13cf34075ed037ebe4ed48d9e
민수 <mandoo4137@gmail.com>
2026-01-24 11:01:42 +0900

    fix : ApplicationCard import

--- apps/student/src/components/club/item/ApplicantCard.tsx
+++ apps/student/src/components/club/item/ApplicantCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import Chip from "../../common/Chip";
+import { Chip } from "../../common/Chip";
 
 interface Applicant {
   name: string;
